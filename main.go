1287bcd001153033e874dab57e453aa4397252ae
Arjun Gopikrishnan <arjun-gopikrishnan>
2023-12-02 19:24:34 +0530

    basic TCP server

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+	"os"
+)
 
 func main() {
 	fmt.Println("Initial setup")
+
+	l, err := net.Listen("tcp", "0.0.0.0:6379")
+
+	if err != nil {
+		fmt.Println("Failed to bind to port 6379", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := l.Accept()
+
+	if err != nil {
+		fmt.Println("Error Accepting connection", err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 }
