ae96c47dbebb4f47b462f57ca820d1787dde303b
Arjun Gopikrishnan <arjun-gopikrishnan>
2023-12-02 19:34:22 +0530

    Added response for PING command

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+func handleConnection(conn net.Conn) {
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		command := scanner.Text()
+
+		if strings.ToUpper(command) == "PING" {
+			conn.Write([]byte("+PONG\r\n"))
+		} else {
+			conn.Write([]byte("Command not recognized\r\n"))
+		}
+
+	}
+}
 
 func main() {
 	fmt.Println("Initial setup")
@@ -16,12 +33,23 @@ func main() {
 		os.Exit(1)
 	}
 
-	conn, err := l.Accept()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Failed to accept connection", err.Error())
+			os.Exit(1)
+		}
 
-	if err != nil {
-		fmt.Println("Error Accepting connection", err.Error())
-		os.Exit(1)
+		go handleConnection(conn)
+		// fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 	}
 
-	fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
+	// conn, err := l.Accept()
+
+	// if err != nil {
+	// 	fmt.Println("Error Accepting connection", err.Error())
+	// 	os.Exit(1)
+	// }
+
+	// fmt.Fprintf(conn, "GET / HTTP/1.0\r\n\r\n")
 }
